987448431f7877a1027fb7856ca98cbec76cd503
yangbing <atefrice@126.com>
2021-01-18 17:58:20 +0800

    更新编译脚本

--- samples/hsm_book_samples/build_sample.sh
+++ samples/hsm_book_samples/build_sample.sh
@@ -10,11 +10,11 @@ export CXX=`which g++`
 cmake -G "Unix Makefiles" -DHSM_DEBUG=ON ..
 make -j
 
-cd $SCRIPT_DIR
-mkdir build_clang
-cd build_clang
-export CC=`which clang`
-export CXX=`which clang++`
-cmake -G "Unix Makefiles" -DHSM_DEBUG=ON ..
-make -j
+#cd $SCRIPT_DIR
+#mkdir build_clang
+#cd build_clang
+#export CC=`which clang`
+#export CXX=`which clang++`
+#cmake -G "Unix Makefiles" -DHSM_DEBUG=ON ..
+#make -j
 
