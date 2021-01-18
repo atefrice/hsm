ea67451396723819f92f02b0637fd04ead7bcb8d
yangbing <atefrice@126.com>
2021-01-18 19:28:50 +0800

    更新 inner entry理解

--- samples/hsm_book_samples/source/ch3/inner_entry_transition.cpp
+++ samples/hsm_book_samples/source/ch3/inner_entry_transition.cpp
@@ -1,4 +1,22 @@
 /*
+flat： add 的时候 1 enter, 
+	   while 而后 1 transition ,  
+	   		1 exit
+			   2 enter
+	即，同 depth 时 只维护一个 state， 一个pop, 另一个才能 push, 直到  no transition
+
+HSM:
+	按照 depth stack 来维护状态， 每次 Process transition 都会 遍历该stack all depth 的 transition
+	进行执行，最后会形成一个 stack,
+	当 stack 中间的 状态变化时， 从 depth 、depth-1 进行 pop, 即此时 exit,
+	而后 push 对应的 新变化的中间state (即此时enter)
+
+	详细可看源码---也是很容易理解
+
+	注意：
+		inner entry 可理解为 默认子状态， 当 inner most状态转换时，还是会先进入到 inner entry, 
+		而后， depth-1 pop , new depth push,----> 再重新执行下 0----> depth的 transition 
+
 
 !!!!!!!!!!!!!!!!!!!
 Alive ...... 
